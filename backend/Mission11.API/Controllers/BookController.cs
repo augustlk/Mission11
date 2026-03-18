d447629daef0a8d8c9ff686cd6047a58047a1e74
augustlk <153124470+augustlk@users.noreply.github.com>
2026-03-18 13:06:46 -0600

    Add project files.

---
+++ backend/Mission11.API/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11.API.Data;
+
+namespace Mission11.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookDbContext _bookContext;
+        public BookController(BookDbContext temp) => _bookContext = temp;
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            var something = _bookContext.Books.ToList();
+            return something;
+        }
+    }
+}
